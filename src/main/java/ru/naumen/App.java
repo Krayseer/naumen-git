da31dd67b4c9e2f9aa3bc6509940d9493b003dbc
Nikita <nikitaburakov08@gmail.com>
2023-10-06 13:19:29 +0500

    Добавил переменную className

--- src/main/java/ru/naumen/App.java
+++ src/main/java/ru/naumen/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         String function = "main";
+        String className = "App";
         System.out.println( "Hello World by Nikita!" );
     }
 }
